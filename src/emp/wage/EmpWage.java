03709dbcc5fc65ad1a6f02f2e566d9b912bc8d8d
megha patil <megharpatil2000@gmail.com>
2022-10-17 21:59:30 +0530

    ADDED:emp dailywage

--- src/emp/wage/EmpWage.java
+++ src/emp/wage/EmpWage.java
@@ -7,14 +7,21 @@ public class EmpWage {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program.");
-		Random random = new Random();
-		int attendance = random.nextInt(2);
-		int PRESENT = 1;
-
-		if (attendance == PRESENT)
-			System.out.println("Employee is Present!");
-		else
-			System.out.println("Employee is Absent!");
-
+		  Random random = new Random();
+	        int attendance = random.nextInt(2);
+	        int PRESENT = 1;               
+	        int WAGE_PER_HOUR = 20;
+	        int fullDayHour = 0;
+	        int dailyWage = 0;
+	        
+	        if (attendance == PRESENT) {
+	            fullDayHour = 8;
+	            System.out.println("Employee is Present!");
+	        }
+	        else
+	            System.out.println("Employee is Absent!");
+	        
+	        dailyWage = WAGE_PER_HOUR * fullDayHour;
+	        System.out.println("Daily wage of employee: " + dailyWage);
 	}
 }
