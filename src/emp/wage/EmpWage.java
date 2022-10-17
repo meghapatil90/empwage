d62aac7bc1e71473f392c3f40332f88830533ab2
megha patil <megharpatil2000@gmail.com>
2022-10-17 21:50:36 +0530

    ADDED:emp attendance

--- src/emp/wage/EmpWage.java
+++ src/emp/wage/EmpWage.java
@@ -1,9 +1,20 @@
 package emp.wage;
 
+import java.util.Random;
+
 public class EmpWage {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to Employee Wage Computation Program.");
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+		int PRESENT = 1;
+
+		if (attendance == PRESENT)
+			System.out.println("Employee is Present!");
+		else
+			System.out.println("Employee is Absent!");
+
 	}
 }
