c6cf661a4cc78087695e939c2b8f0bb494812f85
megha patil <megharpatil2000@gmail.com>
2022-10-17 22:13:55 +0530

    Refactored: using Switch case

--- src/emp/wage/EmpWage.java
+++ src/emp/wage/EmpWage.java
@@ -4,24 +4,33 @@ import java.util.Random;
 
 public class EmpWage {
 
-	public static void main(String[] args) {
+	 static final int PART_TIME = 1;
+	    static final int FULL_TIME = 2;
 
-		System.out.println("Welcome to Employee Wage Computation Program.");
-		  Random random = new Random();
-	        int attendance = random.nextInt(2);
-	        int PRESENT = 1;               
+	    public static void main(String[] args) {
+
+	        System.out.println("Welcome to Emp Wage!");
+	        Random random = new Random();
+	        int attendance = random.nextInt(3);
+
 	        int WAGE_PER_HOUR = 20;
 	        int fullDayHour = 0;
 	        int dailyWage = 0;
-	        
-	        if (attendance == PRESENT) {
-	            fullDayHour = 8;
-	            System.out.println("Employee is Present!");
+
+	        switch (attendance) {
+	            case PART_TIME:
+	                fullDayHour = 4;
+	                System.out.println("Employee is Parttime!");
+	                break;
+	            case FULL_TIME:
+	                fullDayHour = 8;
+	                System.out.println("Employee is Fulltime!");
+	                break;
+	            default:
+	                System.out.println("Employee is Absent!");
 	        }
-	        else
-	            System.out.println("Employee is Absent!");
-	        
 	        dailyWage = WAGE_PER_HOUR * fullDayHour;
 	        System.out.println("Daily wage of employee: " + dailyWage);
+	    }
+
 	}
-}
